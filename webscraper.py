00d1bd616b16daff3f20e8499d3eaaacf80640a3
ashleendaly <Ashleen.Daly@res-group.com>
2023-11-11 16:43:47 +0000

    got urls from csv

--- webscraper.py
+++ webscraper.py
@@ -1,2 +1,18 @@
 import requests
+import csv
+from bs4 import BeautifulSoup
 
+urls = []
+
+with open("GUSS_AI_Target_Webpages.csv", "r") as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    for url in csvreader:
+        urls.append(url[1])
+
+url = [0]
+
+data = requests.get(url)
+html = BeautifulSoup(data.text, "html.parser")
+
+print(html)
